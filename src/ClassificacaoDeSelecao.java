ceaf7f55b3ab879179e17aaaba03d66093d8897c
nicolasgfe <102191266+nicolasgfe@users.noreply.github.com>
2022-11-25 22:26:39 -0300

    adicionando comentarios no projeto

--- src/ClassificacaoDeSelecao.java
+++ src/ClassificacaoDeSelecao.java
@@ -6,16 +6,16 @@ public class ClassificacaoDeSelecao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int valores[] = { 2, 4, 6, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
-		int valorMenor;
-		int indexMenor;
-		String result = "";
+		int valores[] = { 2, 4, 6, 8, 1, 2, 3, 4, 5, 6, 7, 8 }; //Informa manualmente o arrey para fazer a ordenação 
+		int valorMenor; // armazena o menor valor do arrey para fazer a comparação 
+		int indexMenor; // armazena a posição do menor valor do array
+		String result = ""; // Resultado do após a ordenação 
 
-		for (int i = 0; i < valores.length; i++) {
+		for (int i = 0; i < valores.length; i++) { //percorre a variavel {valores} completa
 			valorMenor = valores[i];
 			indexMenor = i;
-			for (int n = i + 1; n < valores.length; n++) {
-				if (valores[n] < valorMenor) {
+			for (int n = i + 1; n < valores.length; n++) {//percorre a variavel {valores} completa, para fazer a comparação entre os valores do array
+				if (valores[n] < valorMenor) { // compara se o o proximo valor é menor que o atual
 					valorMenor = valores[n];
 					indexMenor = n;
 				}
@@ -25,7 +25,7 @@ public class ClassificacaoDeSelecao {
 			valores[indexMenor] = valores[i];
 			valores[i] = valorMenor;
 		}
-		for (int m : valores) {
+		for (int m : valores) { // acria uma variavel com os valores ordenados 
 			result += m + ", ";
 		}
 
